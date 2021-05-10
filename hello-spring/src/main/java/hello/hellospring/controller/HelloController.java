127b93b452bbf0b109ca2be29e7796dad7ca1751
HeeJung Kang <tentasys@gmail.com>
2021-05-10 22:01:55 +0900

    20210510

--- hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,8 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -12,5 +14,41 @@ public class HelloController {
     public String hello(Model model){
             model.addAttribute("data", "spring!!");
             return "hello"; //데이터는 모델로 화면에 넘기고, 리소스 중에 hello인 것을 찾아가서 렌더링 해라. -> hello.html 찾아가자
+    }
+
+    @GetMapping("hello-mvc")
+                                        //required = true가 기본이고, 파라미터를 넘겨야 한다는 의미.
+    public String HelloMVC(@RequestParam(name = "name", required = false) String name, Model model){ //모델이 받으면 그것을 렌더링하는데 사용
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody       //중요함!!
+    //http에서의 body부의 이 데이터를 직접 넣어주겠다는 뜻임. 응답 body부에 이 내용을 직접 넣겠다.
+    public String helloString(@RequestParam("name") String name){
+        return "hello "+name;   //hello spring을 요청한 클라이언트에 그대로 내려보낸다.
+        //템플릿 엔진과의 차이 : View 이런게 없다. 이 문자 그대로 내려감!
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello{
+        private String name;
+
+        //java bean 표준 규약. name을 getter/setter로 접근한다. 프로퍼티 접근 방식이라고도 함.
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
